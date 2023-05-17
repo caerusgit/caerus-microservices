4f5387771d685cb2ab7270210aad13af5c0a77b2
Hugo Alberto Bravo Bonilla <hugo@iMac-Pro.local>
2023-05-17 00:37:39 -0600

    Hasta el video 20, pero no funciona la creación de usuarios POST con postman. Verificar

---
+++ microservices/usuarios/src/main/java/mx/caerus/microservicios/usuarios/services/UsuarioService.java
@@ -0,0 +1,13 @@
+package mx.caerus.microservicios.usuarios.services;
+
+import mx.caerus.microservicios.usuarios.models.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+    List<Usuario> listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar(Usuario usuario);
+    void eliminar(Long id);
+}
